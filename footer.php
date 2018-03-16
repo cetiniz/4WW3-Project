98be5c19e917bc2550f61ca4c4dd6ee739716747
cetiniz <cetiniz@mcmaster.ca>
2018-03-16 00:21:31 -0400

    Added code for header/footer

---
+++ footer.php
@@ -0,0 +1,15 @@
+<footer>
+	<div class="footer-grid">
+		<ul>
+			<li>
+				<a>Legal</a>
+			</li>
+			<li>
+				<a>Disclaimer</a>
+			</li>
+			<li>
+				<a>Author</a>
+			</li>
+		</ul>
+	</div>
+</footer>
---
+++ header.php
@@ -0,0 +1,28 @@
+<header>
+	<div class="logo">
+		<div class="left">
+			<img src="/assets/logo.svg" alt="sites logo: shows a cartoon beaker" style="height:30px"/>
+		</div>
+		<div class="right">
+			<h1>Equip Me</h1>
+		</div>
+	</div>
+	<!-- Navigation menu was created using an encompassing nav element and an unordered list -->
+	<nav class="menu-nav">
+		<button class="menu-bttn">Menu</button>
+		<ul>
+			<li>
+				<a href="/">Home</a>
+			</li>
+			<li>
+				<a href="registration/">Sign Up</a>
+			</li>
+			<li>
+				<a href="/">Login</a>
+			</li>
+			<li>
+				<a href="submission/">Submit</a>
+			</li>
+		</ul>
+	</nav>
+</header>
