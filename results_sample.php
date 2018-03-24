8908ceb9d355ead0b97fececb64238bd0ae15a29
cetiniz <cetiniz@mcmaster.ca>
2018-03-24 19:21:26 -0400

    1.9

--- results_sample.php
+++ results_sample.php
@@ -2,7 +2,7 @@
 <?php 
 	$pdo = new PDO('mysql:host=localhost; dbname=World_Data','cetiniz','$uperC00l');
 	$query = $_POST["query"];
-	$results = $pdo->query("SELECT equipment_owner, equipment_location, equipment_name, equipment_department FROM object_equipment WHERE equipment_name LIKE '%" . $query .  "%'");
+	$results = $pdo->query("SELECT equipment_owner, equipment_location, equipment_name, equipment_department,equipment_id FROM object_equipment WHERE equipment_name LIKE '%" . $query .  "%'");
 ?>
 
  <!DOCTYPE html>
@@ -116,7 +116,7 @@
  								echo '<tr class="table-odd">';
  							}
  							$real_key = $key + 1;
- 							$sample_review = $pdo->query("SELECT review_text FROM object_equipment INNER JOIN object_review ON object_equipment.equipment_id=object_review.equipment_id LIMIT 1");
+ 							$sample_review = $pdo->query("SELECT review_text FROM object_equipment INNER JOIN object_review ON object_equipment.equipment_id=object_review.equipment_id WHERE object_equipment.equipment_id=" . $value[equipment_id] . " LIMIT 1");
  							echo $sample_review[0]['review_text'];
  							echo '<td class="center">' . $real_key . '</td>';
  							echo '<td class="margin-20">';
